45fc6eb5cb66e764d43e0741e78eb2b23112a493
Carlos Lemus <carlosslemus@yahoo.com>
2018-10-04 20:35:48 -0700

    Update compatible classes for MCCs as requested

--- main.py
+++ main.py
@@ -27,9 +27,10 @@ def main():
     # Check Consistency
     #checkConsistency(P,Pf)
     getConsistencyCheck(PLA)
-    #get sets A and B
-    
+
+    #get sets A and B    
     AB = getAB(list_input, partitions, B_size)
+    #AB = {'A': ['x2', 'x4'], 'B': ['x0','x1','x3']}
 
     print ("AB Choosen:", AB)
     print ("\n")
@@ -42,15 +43,17 @@ def main():
 
 
     COM = getCompatabilityClasses(PA,PB,Pf)
+    print ("Compatible Classes:")
     for i in COM:
-        print(i,COM[i])
+        print(i)
 
    
-
-    compatible_list = [(0,1),(0,3),(1,3),(2,3),(2,4),(2,5),(3,4),(3,5),(4,5),(4,6),(4,7),(5,6)]    # Please modify getCompatabilityClasses to have this kind of pairs returned
     #Compatible_list is taken from the lecture slide.
+    compatible_list = [(0,1),(0,3),(1,3),(2,3),(2,4),(2,5),(3,4),(3,5),(4,5),(4,6),(4,7),(5,6)]    # Please modify getCompatabilityClasses to have this kind of pairs returned
+
     
-    MCC = getMCC(compatible_list , B_size)
+    #MCC = getMCC(compatible_list , B_size)    
+    MCC = getMCC(COM , B_size)
     print ("\nMaximum Compatible Classes:")
     for i in MCC:
         print (i)
