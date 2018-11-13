2e10c2836d10fefcc22474a86edd3788c310a246
Carlos Lemus <carlosslemus@yahoo.com>
2018-11-12 19:23:16 -0800

    Add step 1 and change from dictionaries to list

--- encoding.py
+++ encoding.py
@@ -1,85 +1,4 @@
-"""
-Return occurences in a dictionary with appended MCCS
-i.e {0: [3, 4 ,5 ,7]} where 3 is the occurences
-and 4,5,7 is the MCCS it occurs in
-"""
-def getOccurences(MCC, PB):
-    occurs = {}
-    for i in range(len(MCC)):
-        for ii in MCC[i]:
-            for j in PB[ii]:
-                if j in occurs.keys():
-                    occurs[j][0] += 1
-                else:
-                    occurs[j] = [1]
-                occurs[j].append(i)
-    return occurs
-
-def encodeOccurs(occurs, MCC, gray_l, gray_c):
-    MCC_enc = [0]*len(MCC)
-    gray_f = [0]*len(gray_c)
-    #gray_f = [1,0,0,0,0,0,0,0]
-    z = []
-    g_table = [0]*len(occurs)
-    
-
-    #print(MCC_enc)
-    target = 2**(gray_l-1)
-    while(target >= 0):
-        for cube in occurs:
-            if(occurs[cube][0]%2 != 0 and occurs[cube][0] != 1 and cube not in z):
-                z.append(cube)
-                continue
-
-            if(occurs[cube][0] == target):
-##                print(occurs[cube])
-##                print("\n")
-                temp_is_enc = []
-                temp_not_enc = []
-                for i in range(1,len(occurs[cube])): # through MCCs
-                    if(MCC_enc[occurs[cube][i]]): # if MCC is encoded
-                        temp_is_enc.append(occurs[cube][i])
-                    else:
-                        temp_not_enc.append(occurs[cube][i])
-                #print("is enc",temp_is_enc)
-                #print("nt enc",temp_not_enc)
-
-                if(not temp_not_enc): #everything encoded
-                    code = isAdj(MCC_enc[temp_is_enc[0]], temp_is_enc, target/2, MCC_enc)
-                    if(not code): #check if differ by target/2
-                        z.append(cube)
-                    g_table[cube] = code
-                    continue
-                
-
-
-                for CC in temp_not_enc: # have at it, go thru CCs
-                    gotoZ = 1
-                    for i in range(len(gray_c)): # go thru gray list
-                        if(gray_f[i]): # continue if gray not available
-                            continue
-                        else:
-                            code = isAdj(gray_c[i], temp_is_enc, target/2, MCC_enc)
-                            if(code): 
-                                MCC_enc[CC] = gray_c[i]
-                                temp_is_enc.append(CC)
-                                gray_f[i] = 1
-                                g_table[cube] = code
-                                gotoZ = 0
-                                break
-                            else:
-                                continue
-                    if(gotoZ):
-                        z.append(cube)
-                
-                
-        if(target == 0):
-            target = -1
-            
-        target /= 2
-    #print(MCC_enc)
-    
-    return MCC_enc, z, g_table
+import copy
 
 def isAdj(test, refs, diff_l, MCC_enc):
     if(not refs):
@@ -118,6 +37,157 @@ def expand(v):
                 exp[ii].append(copy.deepcopy(v[i]))
     return exp
 
-def step1():
+def split(v):
+    exp = []
+    for i in range(len(v)):
+        if v[i]==2:
+            temp_v = v.copy()
+            temp_v[i] = 0
+            exp.append(temp_v.copy())
+            temp_v[i] = 1
+            exp.append(temp_v.copy())
+    return exp
     
-    expand()
+
+"""
+Return occurences in a dictionary with appended MCCS
+i.e {0: [3, 4 ,5 ,7]} where 3 is the occurences
+and 4,5,7 is the MCCS it occurs in
+"""
+def getOccurences(MCC, PB, N_P):
+    
+    occurs = [[] for _ in range(N_P)]
+    print(occurs)
+    for i in range(len(MCC)):
+        for ii in MCC[i]:
+            for j in PB[ii]:
+                if i in occurs[j]:
+                    continue
+                else:
+                    occurs[j].append(i)
+                
+                   
+    return occurs
+
+def encodeOccurs(occurs, MCC, gray_l, gray_c):
+    MCC_enc = [0]*len(MCC)
+    gray_f = [0]*len(gray_c)
+    #gray_f = [1,0,0,0,0,0,0,0]
+    z = []
+    g_table = [0]*len(occurs)
+    
+
+    #print(MCC_enc)
+    target = 2**(gray_l-1)
+    while(target >= 0):
+        for cube in range(len(occurs)):
+            if(len(occurs[cube])%2 != 0 and len(occurs[cube]) != 1 and cube not in z):
+                z.append(cube)
+                continue
+
+            if(len(occurs[cube]) == target):
+##                print(occurs[cube])
+##                print("\n")
+                temp_is_enc = []
+                temp_not_enc = []
+                for i in range(0,len(occurs[cube])): # through MCCs
+                    if(MCC_enc[occurs[cube][i]]): # if MCC is encoded
+                        temp_is_enc.append(occurs[cube][i])
+                    else:
+                        temp_not_enc.append(occurs[cube][i])
+               # print("is enc",temp_is_enc)
+                #print("nt enc",temp_not_enc)
+
+                if(not temp_not_enc): #everything encoded
+                    code = isAdj(MCC_enc[temp_is_enc[0]], temp_is_enc, target/2, MCC_enc)
+                    if(not code): #check if differ by target/2
+                        z.append(cube)
+                    g_table[cube] = code
+                    continue
+                
+
+
+                for CC in temp_not_enc: # have at it, go thru CCs
+                    gotoZ = 1
+                    for i in range(len(gray_c)): # go thru gray list
+                        if(gray_f[i]): # continue if gray not available
+                            continue
+                        else:
+                            code = isAdj(gray_c[i], temp_is_enc, target/2, MCC_enc)
+                            if(code): 
+                                MCC_enc[CC] = gray_c[i]
+                                temp_is_enc.append(CC)
+                                gray_f[i] = 1
+                                g_table[cube] = code
+                                gotoZ = 0
+                                break
+                            else:
+                                continue
+                    if(gotoZ):
+                        z.append(cube)
+                
+                
+        if(target == 0):
+            target = -1
+            
+        target /= 2
+    #print(MCC_enc)
+    g_table_ret = [[] for _ in range(len(occurs))]
+    for cube in range(len(g_table)):
+        if g_table[cube] == 0:
+            continue
+        else:
+            for bit in (g_table[cube]):
+                   if bit == '0':
+                        g_table_ret[cube].append(0);
+                   if bit == '1':
+                        g_table_ret[cube].append(1);
+                   if bit == '-':
+                        g_table_ret[cube].append(2); 
+    return MCC_enc, z, g_table_ret
+
+
+def step1(b_table, z, g_table):
+    print("-----------STEP1--------------","\n")
+    temp_z = copy.deepcopy(z)
+    for cube in z:
+        s = split(b_table[cube])
+        for i in range(0,len(s),2):
+            if s[i] in b_table and s[i+1] in b_table:
+                if(g_table[b_table.index(s[i])] and g_table[b_table.index(s[i+1])]):
+                    z.remove(cube)
+    print("-----------STEP1---Done-----------","\n")  
+    return z
+    
+    
+def step2(z,prodCC,g_table,cc_code,g_code):
+    print("-----------STEP2--------------","\n")
+    #print("Z Set:",z)
+    #print("Prod CCs:",prodCC)
+    #print("G table",g_table)
+    g_table1 = copy.deepcopy(g_table)
+    g_code1 = copy.deepcopy(g_code)
+    z1 = copy.deepcopy(z)
+    for i in z:
+        subcubes = expand(g_table[i])
+        subcode = []
+        for j in subcubes:
+            for k in range(0,len(prodCC)):
+                for l in prodCC[k]:
+                    subcubes_prodcc = expand(l)
+                    if(j in subcubes_prodcc):
+                        subcode.append(cc_code[k])
+        list2 = [x for x in subcode if x]
+        subcode = list2
+        if(len(subcode)==len(subcubes)):
+            print (i,z1,subcubes,subcode)
+            g_table1[i] = subcubes[0]
+            g_code1[i] = subcode[0]
+
+            for m in range(1,len(subcubes)):
+                g_table1.update({len(g_table1) : subcubes[m]})
+                g_code1.update({len(g_code1) : subcode[m]})
+            z1.remove(i)
+    print("-----------STEP2---Done-----------","\n")  
+    return z1,g_table1,g_code1
+
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from consistencyCheck import *
 from compatabilityClasses import *
 import math
 import operator
-import copy
        
 
 def main():
@@ -40,24 +39,27 @@ def main():
     
     PA = getPartitionGroup(AB['A'], partitions)
     PB = getPartitionGroup(AB['B'], partitions)
-    PB = remove_(PB)
 
-    s = []
-
-    """
-    Removing duplicate or subsets in PB
-    """
-    for i in range(0,len(PB)):
-        flag = 0
-        for j in range(0,len(PB)):
-            if (i!=j):
-                if(PB[i].issubset(PB[j])):
-                    flag = 1
-        if(flag == 0):
-            s.append(PB[i])
-
-    PB = s
-
+    print(partitions)
+##    tempPB = PB
+##    PB = remove_(PB)
+##
+##    s = []
+##
+##    """
+##    Removing duplicate or subsets in PB
+##    """
+##    for i in range(0,len(PB)):
+##        flag = 0
+##        for j in range(0,len(PB)):
+##            if (i!=j):
+##                if(PB[i].issubset(PB[j])):
+##                    flag = 1
+##        if(flag == 0):
+##            s.append(PB[i])
+##
+##    PB = s
+    
                 
     print ("Set A: ",  PA)
     print ("Set B: ",  PB)
@@ -80,8 +82,7 @@ def main():
        print (i)
 
 
-    print ()
-    occurs = getOccurences(MCC, PB)
+    occurs = getOccurences(MCC, PB, PLA["N_P"])
     print ("\nOccurances:")
     print(occurs)
 
@@ -92,31 +93,41 @@ def main():
 
  
     MCC_enc, z, g_table = encodeOccurs(occurs, MCC, gray_l, gray_c)
-    print ("\nEncoded MCCs:")
+    print ("\nEncoded MCCs after encoding                       :")
     print(MCC_enc)
-    print("\n")
     
-    print("Z set:")
-    print(z)
-    print("\n")
+    print("\n Z set after encoding:")
 
-    print("g table set:")
+    print("\n g table set after encoding:")
     print(g_table)
 
-    #step1();
+    
 
     print ("\nMaximum Compatible Classes:",MCC)
 
     
+    
+    
+
+
+   # print ("Product of CCs::",prodCC)
+    b_table =  [[2,2,0,2],[2,2,0,1],[2,1,2,2],[2,0,0,1],[1,2,0,0],[2,1,0,2],[2,1,1,0],
+                [2,0,1,1],[0,0,0,2],[2,1,1,1],[0,0,1,1],[2,0,0,0],[2,1,1,0],[2,2,1,0],
+                [2,0,1,0],[1,2,1,2],[2,2,1,0]]
+    z = step1( b_table, z, g_table)
+
+    print("\nZ tables step 1 removed:" , z)
+
     z = [2,4,8,13,16] # -15
-    g_table = {0: [2,2,0,2], 1:[2,2,0,1], 2:[2,1,2,2],3:[2,0,0,1],4:[1,2,0,0],5:[2,1,0,2],6:[2,1,1,0],7:[2,0,1,1],8:[0,0,0,2],9:[2,1,1,1],10:[0,0,1,1],11:[2,0,0,0],12:[2,1,1,0],13:[2,2,1,0],14:[2,0,1,0],15:[1,2,1,2],16:[2,2,1,0]}
+    b_table = {0: [2,2,0,2], 1:[2,2,0,1], 2:[2,1,2,2],3:[2,0,0,1],4:[1,2,0,0],5:[2,1,0,2],6:[2,1,1,0],7:[2,0,1,1],8:[0,0,0,2],9:[2,1,1,1],10:[0,0,1,1],11:[2,0,0,0],12:[2,1,1,0],13:[2,2,1,0],14:[2,0,1,0],15:[1,2,1,2],16:[2,2,1,0]}
     g_code = {0: [0,2,2], 1:[0,0,2], 2:[],3:[0,0,1],4:[],5:[0,0,0],6:[1,1,1],7:[1,0,2],8:[],9:[0,0,1],10:[1,0,0],11:[0,1,2],12:[1,1,1],13:[],14:[0,0,1],15:[],16:[]}
     cc_B = [[2],[6],[0],[4],[9,5,1],[8],[3,7]]
     cc = [(7,10),(7,15),(0,8,11),(0,4,11),(0,1,2,3,8,9,13,14,15,16),(2,6,12,13,15,16),(0,1,2,4,5)]
     cc_code = [[1,0,0],[1,0,1],[0,1,0],[0,1,1],[0,0,1],[1,1,1],[0,0,0]]
     gray = [[1,1,0]]
 
-    prodPB = getProdPB(PB,g_table)
+    
+    prodPB = getProdPB(PB,b_table)
 
     prodCC = {}
 
@@ -127,44 +138,10 @@ def main():
         prodCC[cc_B.index(i)] = temp 
 
 
-   # print ("Product of CCs::",prodCC)   
-
-    z,g_table,g_code = step2(z,prodCC,g_table,cc_code,g_code)
+    z,g_table,g_code = step2(z,prodCC,b_table,cc_code,g_code)
 
     print (z,"\n",g_table,"\n",g_code)
 
-
-    
-def step2(z,prodCC,g_table,cc_code,g_code):
-    print("-----------STEP2--------------","\n")
-    #print("Z Set:",z)
-    #print("Prod CCs:",prodCC)
-    #print("G table",g_table)
-    g_table1 = copy.deepcopy(g_table)
-    g_code1 = copy.deepcopy(g_code)
-    z1 = copy.deepcopy(z)
-    for i in z:
-        subcubes = expand(g_table[i])
-        subcode = []
-        for j in subcubes:
-            for k in range(0,len(prodCC)):
-                for l in prodCC[k]:
-                    subcubes_prodcc = expand(l)
-                    if(j in subcubes_prodcc):
-                        subcode.append(cc_code[k])
-        list2 = [x for x in subcode if x]
-        subcode = list2
-        if(len(subcode)==len(subcubes)):
-            print (i,z1,subcubes,subcode)
-            g_table1[i] = subcubes[0]
-            g_code1[i] = subcode[0]
-
-            for m in range(1,len(subcubes)):
-                g_table1.update({len(g_table1) : subcubes[m]})
-                g_code1.update({len(g_code1) : subcode[m]})
-            z1.remove(i)
-    print("-----------STEP2---Done-----------","\n")  
-    return z1,g_table1,g_code1
 
         
 
